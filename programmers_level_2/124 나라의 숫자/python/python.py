426521cd62285244a08b5f11e3170eec2d5066a3
googoocone1 <googoocone1@naver.com>
2024-07-15 20:30:35 +0900

    124 나라의 숫자 python

---
+++ programmers_level_2/124 나라의 숫자/python/python.py
@@ -0,0 +1,39 @@
+def solution(n) :
+  answer = ''
+  
+  sum = 0
+  sum_1 = 0
+  num = 1
+  list =  []
+  
+  
+  while True:
+    sum = sum + 3**(num)
+    
+    if n <= sum :
+      break
+    else :
+      num += 1
+    sum_1 = sum
+
+  gap = n - sum_1 -1
+      
+  while gap :
+    list.append(gap % 3)
+    gap = gap //3
+    
+  start = ['1']*num
+  print(list)
+ 
+  for i in range(len(list)):
+    print('이거', list[i])
+    if list[i] == 1 :
+      start[i] = '2'
+    elif list[i] == 2:
+      start[i] = '4'
+
+  
+  start = ''.join(start)[::-1]
+  print(start)
+  
+solution(41)
